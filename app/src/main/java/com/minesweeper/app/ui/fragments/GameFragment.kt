cebef47dba55e5f4e43516dc76efcf1b1559c4e0
Louis Lefevre <louislefev@gmail.com>
2021-12-28 02:47:59 +0000

    Add board tile generation
    Generate an empty board based on the specified rows and columns.
    
    Also added ZoomLayout for navigating larger boards.

--- app/src/main/java/com/minesweeper/app/ui/fragments/GameFragment.kt
+++ app/src/main/java/com/minesweeper/app/ui/fragments/GameFragment.kt
@@ -4,16 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TableRow
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.minesweeper.app.R
 import com.minesweeper.app.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
     private lateinit var binding: FragmentGameBinding
+    private val navArgs: GameFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBoardTable(navArgs.rows, navArgs.columns)
+    }
+
+    private fun setupBoardTable(rows: Int, columns: Int) {
+        for (j in 0 until rows) {
+            val row = layoutInflater.inflate(R.layout.board_row, null) as TableRow
+
+            for (i in 0 until columns) {
+                row.addView(layoutInflater.inflate(R.layout.board_tile, null) as ImageButton)
+            }
+
+            binding.tlBoard.addView(row)
+        }
     }
 }
