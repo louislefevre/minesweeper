e529e9dd7f46aed84c22725af91ee1ed935baed8
Louis Lefevre <louislefev@gmail.com>
2022-01-01 16:51:34 +0000

    Enable action bar
    Action bar is enabled for all fragments, except GameFragment.

--- app/src/main/java/com/minesweeper/app/ui/MainActivity.kt
+++ app/src/main/java/com/minesweeper/app/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.minesweeper.app.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.minesweeper.app.R
 import com.minesweeper.app.databinding.ActivityMainBinding
@@ -20,9 +23,28 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvFragmentHost) as NavHostFragment
         navController = navHostFragment.navController
+        setupActionBar()
 
         binding.apply {
             bnvNavBar.setupWithNavController(navController)
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun setupActionBar() {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.navStatisticsFragment, R.id.navMenuFragment, R.id.navSettingsFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navGameFragment -> {
+                    supportActionBar?.setShowHideAnimationEnabled(false)
+                    supportActionBar?.hide()
+                }
+                else -> supportActionBar?.show()
+            }
         }
     }
 }
